79ff85acd2f6d641bbae2c957f44a71362ac1e15
haim.magen@gmail.com <haim.magen@gmail.com@3d1efa0c-ed9f-d9e0-38c0-677d1c649a7d>
2011-03-31 12:18:40 +0000

    added support for rtl

--- Default.master.cs
+++ Default.master.cs
@@ -3,7 +3,7 @@ using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.WebControls;
 using System.Web;
 
-public partial class MasterPage : System.Web.UI.MasterPage
+public partial class MasterPage1 : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
